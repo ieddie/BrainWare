857e0b5b08ced0488f0f4fc3917dc020612f3433
edk <ekantsevich@uplandsoftware.com>
2020-01-08 23:24:58 -0500

    Main technical goal here was to make sure the sql connection is closed after the reader is no longer  needed. Additionally, introducing DAL layer and hiding the sql details from the service.

--- Web/Infrastructure/Database.cs
+++ Web/Infrastructure/Database.cs
@@ -1,40 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using Web.Models;
 
 namespace Web.Infrastructure
 {
+    using System.Data;
     using System.Data.Common;
     using System.Data.SqlClient;
 
-    public class Database
+    public static class Database
     {
-        private readonly SqlConnection _connection;
+        private static string _connectionStr = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename=D:\tmp\ieddie\BrainWare\Web\App_Data\BrainWare.mdf";
 
-        public Database()
+        public static List<Order> GetOrderDetails()
         {
-            _connection = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename=D:\\BrainWare\\Web\\App_Data\\BrainWare.mdf");
+            // Get the orders
+            var query = "SELECT c.name, o.description, o.order_id FROM company c INNER JOIN [order] o on c.company_id=o.company_id";
 
-            _connection.Open();
+            List<Order> result = new List<Order>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionStr))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Call Read before accessing data.
+                        while (reader.Read())
+                        {
+                            var row = (IDataRecord)reader;
+
+                            result.Add(new Order(row.GetString(0), row.GetString(1), row.GetInt32(2)));
+                        }
+
+                        // Call Close when done reading.
+                        reader.Close();
+                    }
+                }
+            }
+
+            return result;
         }
 
-
-        public DbDataReader ExecuteReader(string query)
+        public static List<OrderProduct> GetOrderProducts()
         {
-           
+            // Get the orders
+            var query =
+                @"SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price 
+                FROM orderproduct op 
+                    INNER JOIN product p on op.product_id=p.product_id";
 
-            var sqlQuery = new SqlCommand(query, _connection);
+            List<OrderProduct> result = new List<OrderProduct>();
 
-            return sqlQuery.ExecuteReader();
+            using (SqlConnection connection = new SqlConnection(_connectionStr))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = (IDataRecord)reader;
+                            result.Add(new OrderProduct(row.GetInt32(1), row.GetInt32(2), row.GetDecimal(0), row.GetInt32(3), row.GetString(4), row.GetDecimal(5)));
+                        }
+                        reader.Close();
+                    }
+                }
+            }
+
+            return result;
         }
-
-        public int ExecuteNonQuery(string query)
-        {
-            var sqlQuery = new SqlCommand(query, _connection);
-
-            return sqlQuery.ExecuteNonQuery();
-        }
-
+        
     }
 }
--- Web/Models/Order.cs
+++ Web/Models/Order.cs
@@ -19,21 +19,43 @@ namespace Web.Models
 
         public List<OrderProduct> OrderProducts { get; set; }
 
-    }
+        public Order()
+        {
+            OrderProducts = new List<OrderProduct>();
+        }
 
+        public Order(string companyName, string description, int orderId)
+        {
+            CompanyName = companyName;
+            Description = description;
+            OrderId = orderId;
+            OrderTotal = 0;
+            OrderProducts = new List<OrderProduct>();
+        }
+    }
 
     public class OrderProduct
     {
-        public int OrderId { get; set; }
+        public Int32 OrderId { get; set; }
 
-        public int ProductId { get; set; }
+        public Int32 ProductId { get; set; }
 
         public Product Product { get; set; }
     
-        public int Quantity { get; set; }
+        public Int32 Quantity { get; set; }
 
         public decimal Price { get; set; }
 
+        public OrderProduct() { }
+
+        public OrderProduct(Int32 orderId, Int32 productId, decimal price, Int32 quatity, string productName, decimal originalProductPrice)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quatity;
+            Price = price;
+            Product = new Product() { Name = productName, Price = originalProductPrice };
+        }
     }
 
     public class Product
